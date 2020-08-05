9412d5700e28b76ed2af8425eb9545047440d337
Taufiqurrahman <54893241+Taufiqurrahman04@users.noreply.github.com>
2020-08-05 16:17:10 +0700

    Thread Pool Queue User Work Item

---
+++ ThreadPool.QueueUserWorkItem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadPool.QueueUserWorkItem
+{
+    class Program
+    {
+        
+        static void methodTest(object d)
+        {
+            Console.WriteLine("Thread {0}",(int)d);
+        }
+        static void Main(string[] args)
+        {
+            var reset = new ManualResetEvent(false);
+            for(int x = 0; x < 10; x++)
+            {
+                System.Threading.ThreadPool.QueueUserWorkItem(new WaitCallback( methodTest), x);
+            }
+            Thread.Sleep(100);
+            Console.Write("Press Enter To Reset Thread Pool ? ");
+            Console.ReadLine();
+            reset.Set();
+        }
+    }
+}
