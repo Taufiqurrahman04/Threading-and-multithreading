6cd85d14e6b3f3e8c4973e0b20c40f415021790c
Taufiqurrahman <54893241+Taufiqurrahman04@users.noreply.github.com>
2020-08-05 16:14:29 +0700

    Task Parallel Library

---
+++ TaskParallelLibrary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskParallelLibrary
+{
+    class Program
+    {
+        static void Run()
+        {
+            Thread.Sleep(10000);
+        }
+        static void UseTPL()
+        {
+            var stopwacth = Stopwatch.StartNew();
+            Console.WriteLine("Mulai : {0} ", stopwacth.ElapsedMilliseconds);
+            //--Memanggil Class Parallel Untuk Sekali Looping
+            Parallel.For(0, 4, x => Run());
+            Console.WriteLine("Finish : {0} ", stopwacth.ElapsedMilliseconds);
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            UseTPL();
+            Console.ReadLine();
+        }
+    }
+}
